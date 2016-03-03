eeb59b42ff2f4c1ca974a5bf280d3529e8bb79cb
c03rcion <areeves9@icloud.com>
2016-03-02 19:37:58 -0600

    added model to admin file, used register() to bring model to admin site

--- mysite/strain_review/admin.py
+++ mysite/strain_review/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Strain, Review
+
 # Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    list_display = ('strain', 'rating', 'user_name', 'comment', 'pub_date')
+    list_filter = ['pub_date', 'user_name']
+    search_fields = ['comment']
+
+admin.site.register(Strain)
+admin.site.register(Review, ReviewAdmin)
