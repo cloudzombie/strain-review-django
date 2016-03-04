973d0ed864bfd06620d5cb0b6e85d70ffe289867
c03rcion <areeves9@icloud.com>
2016-03-04 00:24:47 -0600

    added registration button and can take new users now

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jquery',
     'bootstrap3',
     'strain_review',
     'registration',
