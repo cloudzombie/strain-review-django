fec2562ab66289cc4648b6197697b38374cfb555
c03rcion <areeves9@icloud.com>
2016-03-03 20:51:49 -0600

    added navbar to base template

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^strain_review/', include('strain_review.urls', namespace="strain_review")),
     url(r'^admin/', admin.site.urls),
+    url('^accounts/', include('django.contrib.auth.urls')),
 ]
