cbb8e31a408dca98707b5a34c6027ec133502bfb
c03rcion <areeves9@icloud.com>
2016-03-16 18:56:49 -0500

    set static root in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/strain_review/review/user'
 
